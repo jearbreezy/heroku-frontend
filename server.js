7aa9e62530724f234ec1dd1f859a04ef9d07bc1b
metaljearsolid <42151720+metaljearsolid@users.noreply.github.com>
2019-02-11 02:28:20 +0800

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port =process.env.PORT || 8080
 
 app.get("/", (req, res) => {
   res.render("index", {
-    title: " Welcome to JearBreezy's Testing Envi",
+    title: " Welcome to my Testing environment!",
     people: people.profiles
   });
 });
