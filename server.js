824cba427853e3ca06a49d68ca28ffe1f11ba43a
jearbreezy <33287606+jearbreezy@users.noreply.github.com>
2019-03-15 20:16:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port =process.env.PORT || 8080
 
 app.get("/", (req, res) => {
   res.render("index", {
-    title: "Welcome to JearBreezy's Testing Envi ni Jear",
+    title: "Welcome to JearBreezy's Testing Envi!",
     people: people.profiles
   });
 });
